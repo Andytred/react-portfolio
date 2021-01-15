8645ec15e5c9e4f5d6a0c7f8c02ef683aee9dfbd
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-01-15 20:54:46 +0100

    Adding setState and click button show less and show more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+    this.readMore = this.readMore.bind(this);
+    this.showLess = this.showLess.bind(this);
+  }
+
+  
+
+  readMore () {
+    // this.state.displayBio = true;
+    this.setState ({ displayBio: true });
+  }
+
+  showLess () {
+    this.setState ({ displayBio: false });
+  }
+
+  render() {
+    // if (!this.state.displayBio) {
+    //   bio = null;
+    // }
+
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>My name is Andrea, I'm a service designer and Junior front-end developer</p>
+        {
+         this.state.displayBio ? (
+        <div>
+          <p>I live in Barcelona, and code every day.</p>
+          <p>My favorite language is JS, and learning React.js</p>
+          <p>Besides coding, I also have a passion for service design and implementing service desing thinking</p>
+          <button onClick={this.showLess}>Show less</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={this.readMore}>Read more</button>
+        </div>
+      )
+        }
+      </div>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
 
 export default App;
